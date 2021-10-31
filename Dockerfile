88dfa3daf01c5120723bcabdaf558c00e008fef8
MykolaM-AVISPL <59696595+MykolaM-AVISPL@users.noreply.github.com>
2021-10-31 17:04:11 +0200

    docker with vm options

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM openjdk:14.0.2-oracle
 VOLUME ["/application.properties"]
+ENV JAVA_OPTS "-XX:+UseG1GC"
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 
-ENTRYPOINT ["java", "-jar", "/app.jar","--spring.config.location=/application.yml"]
+ENTRYPOINT ["sh", "-c","java $JAVA_OPTS -jar /app.jar --spring.config.location=/application.yml"]
--- src/main/kotlin/com/k8s/example/shop/rest/TestController.kt
+++ src/main/kotlin/com/k8s/example/shop/rest/TestController.kt
@@ -1,5 +1,7 @@
 package com.k8s.example.shop.rest
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.availability.AvailabilityChangeEvent
 import org.springframework.boot.availability.LivenessState
 import org.springframework.boot.availability.ReadinessState
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.management.ManagementFactory
 import java.util.*
+import javax.annotation.PostConstruct
 
 
 @RestController
 class TestController(private val eventPublisher: ApplicationEventPublisher) {
+    var logger: Logger = LoggerFactory.getLogger(TestController::class.java)
 
     @GetMapping("/messages")
     fun getMessage(): String {
@@ -27,9 +32,15 @@ class TestController(private val eventPublisher: ApplicationEventPublisher) {
         when (version) {
             "broken" -> AvailabilityChangeEvent.publish(this.eventPublisher, IllegalArgumentException("no no"), LivenessState.BROKEN)
             "refusing" -> AvailabilityChangeEvent.publish(this.eventPublisher, IllegalArgumentException("no no"), ReadinessState.REFUSING_TRAFFIC)
-            else -> {
-            }
+            else -> {}
         }
         return "done";
+    }
+
+    @PostConstruct
+    fun params(){
+        val runtimeMxBean = ManagementFactory.getRuntimeMXBean()
+        val arguments = runtimeMxBean.inputArguments
+        arguments.forEach { logger.warn(it) }
     }
 }
